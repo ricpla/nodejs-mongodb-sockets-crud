60076b5d7af675e72da051709a9d843f71c0ee50
Ricpla <ricpla17@gmail.com>
2022-07-31 00:01:21 -0400

    Changes 2

--- src/db.js
+++ src/db.js
@@ -1,8 +1,9 @@
 import { connect } from 'mongoose'
+import { MONGODB_URI } from './config'
 
 export const connectDB = async () => {
     try {
-        await connect('')
+        await connect(MONGODB_URI)
         console.log('connect to DB');
     } catch (error) {
         console.error(error);
